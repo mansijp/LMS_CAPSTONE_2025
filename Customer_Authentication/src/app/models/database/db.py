a271898f5af0a1a6c9da6879ef53ecd9552c4da5
Mansi Patel <45537538+mansijp@users.noreply.github.com>
2025-06-02 14:18:01 -0400

    Database connection fix

--- Customer_Authentication/src/app/models/database/db.py
+++ Customer_Authentication/src/app/models/database/db.py
@@ -3,10 +3,11 @@ from dotenv import load_dotenv
 import os
 
 # Load environment variables
-load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '../../../../.env'))
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '../../../../../.env'))
 
 # MongoDB Connection (pymongo)
-MONGO_URL = os.getenv("MONGO_URL")
+MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://lmscapstonemansi:capstonemansi25@primary.ncu4vwq.mongodb.net/")
+
 client = MongoClient(MONGO_URL)
 db = client["sample_mflix"]
 
--- Search/src/app/models/database/db.py
+++ Search/src/app/models/database/db.py
@@ -6,7 +6,7 @@ import os
 load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '../../../../.env'))
 
 # MongoDB Connection (pymongo)
-MONGO_URL = os.getenv("MONGO_URL")
+MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://lmscapstonemansi:capstonemansi25@primary.ncu4vwq.mongodb.net/")
 client = MongoClient(MONGO_URL)
 db = client["sample_mflix"]
 
